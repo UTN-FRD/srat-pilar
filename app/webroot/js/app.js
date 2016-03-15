dcfc28bd41ff23202f7550c0ad1e80ef95d44da4
Jorge Alberto Cricelli <jacricelli@gmail.com>
2016-03-15 20:06:43 -0300

    Refactorizado cierre de sesión por inactividad.

--- app/webroot/js/app.js
+++ app/webroot/js/app.js
@@ -11,18 +11,27 @@
  * @author Jorge Alberto Cricelli <jacricelli@gmail.com>
  */
 
-$(function() {
-	if (!$('#UsuarioLoginForm, .admin-users').length) {
+$( function() {
+	if ( $( "a.logout" ).length === 1 && $( ".admin-users" ).length === 0 ) {
 		var idleCounter = 0,
-		idleInterval = window.setInterval(function() {
-			if (++idleCounter >= 120) {
-				window.clearInterval(idleInterval);
-				window.location = $('a.logout').attr('href');
+		idleInterval = window.setInterval( function() {
+			if ( ++idleCounter >= 120 ) {
+				window.clearInterval( idleInterval );
+				window.location = $( "a.logout" ).attr( "href" );
 			}
-		}, 1000);
-	}
+		}, 1000 );
 
-	$(document).on('keypress mousedown mousemove scroll wheel', function() {
-		idleCounter = 0;
-	});
-});
+		$( document ).on( "keypress mousedown scroll wheel", function() {
+			idleCounter = 0;
+		} );
+
+		var lastClientX = 0, lastClientY = 0;
+		$( document ).on( "mousemove", function( e ) {
+			if ( lastClientX !== e.clientX || lastClientY !== e.clientY ) {
+				idleCounter = 0;
+			}
+			lastClientX = e.clientX;
+			lastClientY = e.clientY;
+		} );
+	}
+} );
